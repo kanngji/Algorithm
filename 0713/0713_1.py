88b4563a90f4db4509e2946a19e8ca5a0b4162fb
kanngji <50470748+kanngji@users.noreply.github.com>
2022-07-13 02:18:26 +0900

    변수입력과 연산자

---
+++ 0713/0713_1.py
@@ -0,0 +1,35 @@
+'''
+변수입력과
+연산자
+'''
+
+# a=input("숫자를 입력하세요")
+# print(a)
+
+# split함수는 a,b 를 분리해서 처음꺼를 a 나중꺼를 b에 넣어준다
+# a,b가 string이라는 것을 알수있따.
+# a,b = input("숫자를 입력하세요 :").split()
+
+#숫자로 받으려면
+# a,b=map(int, input("숫자를 입력해주세요").split())
+# print(a+b)
+
+#int형으로 바꾸기
+# num = input()
+# num = int(num)
+# print(type(num))
+
+# input().split() 에서 두 문자열을 받은후 
+# int로 바꿔진다
+# a,b = map(int, input().split())
+
+# // 는 몫을 구한다
+# % 는 나머지를 구한다
+# **는 거듭제곱이다 ex) 2**3 는 2의 3제곱 = 8
+
+
+# 실수 + 정수는 실수가 나온다
+# a=4.3
+# b=5
+# c=a+b
+# print(type(c))
